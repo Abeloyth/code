7a877ae44014da9d3a74cd19467b266e9fe5fa11
Abeloyth <115855056+Abeloyth@users.noreply.github.com>
2022-10-15 20:59:08 +0800

    Update P1001.cpp

--- c++/P1001.cpp
+++ c++/P1001.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+#define I long long a, b;
+#define AK cin >> a >> b;
+#define IOI cout << a + b;
+#define exit return 0;
+
+int main()
+{
+	I
+	AK
+	IOI
+	exit
+}
